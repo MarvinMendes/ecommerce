8f7b863824478f630c328b2252a8adbf7688c953
Marvin Mendes <marvin.m.lopes@gmail.com>
2020-06-24 07:06:53 -0300

    Removendo pastas

--- target/classes/META-INF/banco-de-dados/dados-iniciais.sql
+++
@@ -1,5 +0,0 @@
-use algaworks_ecommerce;
-insert into product (id, product_name, product_value, product_description) values (1, 'Kindle', 499.0, 'Conheça o novo Kindle, agora com iluminação embutida ajustável, que permite que você leia em ambientes abertos ou fechados, a qualquer hora do dia.');
-insert into product (id, product_name, product_value, product_description) values (3, 'Nikon', 1500.0, 'Uma nova perspectiva para suas fotos.');
-insert into client (id, name) values (2, 'Mario Jorge');
-insert into client (id, name) values (3, 'Marcelo Mendes Lopes');
