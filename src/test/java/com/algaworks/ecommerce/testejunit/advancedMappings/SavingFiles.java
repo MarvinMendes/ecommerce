3e6d66643c73f54524a1306614d03304c039ef22
Marvin Mendes <marvin.m.lopes@gmail.com>
2020-07-21 00:48:05 -0300

    Mapeando e persistindo dados de arquivos com @Lob

---
+++ src/test/java/com/algaworks/ecommerce/testejunit/advancedMappings/SavingFiles.java
@@ -0,0 +1,45 @@
+package com.algaworks.ecommerce.testejunit.advancedMappings;
+
+import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.model.order.Invoice;
+import com.algaworks.ecommerce.model.order.Order;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.Date;
+
+public class SavingFiles extends EntityManagerTest {
+
+    @Test
+    public void saveInvoice() {
+        em.getTransaction().begin();
+        Order order = em.find(Order.class, 1);
+
+        Invoice invoice = new Invoice();
+        invoice.setEmissionDate(new Date());
+        invoice.setOrder(order);
+        invoice.setXml(loadInvoice());
+        em.persist(invoice);
+        em.getTransaction().commit();
+
+        em.clear();
+
+        Order orderSaved = em.find(Order.class, order.getId());
+        Invoice invoiceSaved = em.find(Invoice.class, order.getId());
+
+        Assert.assertNotNull(orderSaved.getInvoice().getXml());
+        Assert.assertNotNull(invoiceSaved.getXml());
+
+
+    }
+
+
+    protected static byte[] loadInvoice() {
+        try {
+            return SavingFiles.class.getResourceAsStream("/invoice.xml").readAllBytes();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
