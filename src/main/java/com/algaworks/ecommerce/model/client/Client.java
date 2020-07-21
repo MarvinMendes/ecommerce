2d00f5e624aea0c098e9e0f499edea7f797299cc
Marvin Mendes <marvin.m.lopes@gmail.com>
2020-07-21 13:03:30 -0300

    Mapeando tabela secundária com @SecondaryTable

--- src/main/java/com/algaworks/ecommerce/model/client/Client.java
+++ src/main/java/com/algaworks/ecommerce/model/client/Client.java
@@ -7,12 +7,15 @@ import lombok.Setter;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.Date;
+import java.util.Map;
 import java.util.Set;
 
 @Entity
 @Getter
 @Setter
 @Table(name = "client")
+@SecondaryTable(name = "client_details", pkJoinColumns = @PrimaryKeyJoinColumn(name = "client_id"))
 public class Client implements Serializable {
 
     private static final long serialVersionUID = 5378753649905055112L;
@@ -23,8 +26,16 @@ public class Client implements Serializable {
     private Integer id;
     @Column(name = "client_name")
     private String name;
+    @Column(table = "client_details")
     @Enumerated(EnumType.STRING)
     private Gender gender;
+    @Column(name = "date_of_birth", table = "client_details")
+    private Date dateOfBirth;
     @OneToMany(mappedBy = "client")
     private Set<Order> order;
+    @ElementCollection
+    @MapKeyColumn(name = "type")
+    @Column(name = "description")
+    @CollectionTable(name = "contact_client", joinColumns = @JoinColumn(name = "client_id"))
+    private Map<String, String> contact;
 }
