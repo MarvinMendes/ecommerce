cbff59dde81693e365f85381f8d00ac39451eff5
Marvin Mendes <marvin.m.lopes@gmail.com>
2020-07-28 19:16:38 -0300

    Configurando detalhes da tabela com @Table

---
+++ src/test/java/com/algaworks/ecommerce/testejunit/advancedMappings/DllTest.java
@@ -0,0 +1,13 @@
+package com.algaworks.ecommerce.testejunit.advancedMappings;
+
+import com.algaworks.ecommerce.EntityManagerTest;
+import org.junit.Test;
+
+public class DllTest extends EntityManagerTest {
+
+
+    @Test
+    public void creatingDll() {
+
+    }
+}
