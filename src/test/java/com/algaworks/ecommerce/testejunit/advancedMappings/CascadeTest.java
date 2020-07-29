b41020f5618240781325be64fef5d26bd01dae0d
Marvin Mendes <marvin.m.lopes@gmail.com>
2020-07-29 02:15:08 -0300

    Fazendo inserções com objetos em cascata

---
+++ src/test/java/com/algaworks/ecommerce/testejunit/advancedMappings/CascadeTest.java
@@ -0,0 +1,18 @@
+package com.algaworks.ecommerce.testejunit.advancedMappings;
+
+import com.algaworks.ecommerce.EntityManagerTest;
+import org.junit.Test;
+
+public class CascadeTest extends EntityManagerTest {
+
+    @Test
+    public void testCascade() {
+
+
+
+
+
+    }
+
+
+}
