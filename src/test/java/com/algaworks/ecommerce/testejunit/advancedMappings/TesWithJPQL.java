1221c54416526917f59076ad92e634a35f5a610b
Marvin Mendes <marvin.m.lopes@gmail.com>
2020-08-06 08:23:03 -0300

    Selecionando um atributo da entidade como retorno da consulta

--- src/test/java/com/algaworks/ecommerce/testejunit/advancedMappings/TesWithJPQL.java
+++ src/test/java/com/algaworks/ecommerce/testejunit/advancedMappings/TesWithJPQL.java
@@ -1,12 +1,15 @@
 package com.algaworks.ecommerce.testejunit.advancedMappings;
 
 import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.model.client.Client;
 import com.algaworks.ecommerce.model.order.Order;
+import com.algaworks.ecommerce.model.product.Product;
 import org.hibernate.annotations.GenerationTime;
 import org.junit.Assert;
 import org.junit.Test;
 
 import javax.persistence.TypedQuery;
+import java.util.List;
 
 public class TesWithJPQL extends EntityManagerTest {
 
@@ -19,7 +22,20 @@ public class TesWithJPQL extends EntityManagerTest {
         Order singleResult = typedQuery.getSingleResult();
 
         Assert.assertNotNull(singleResult);
+    }
 
+    @Test
+    public void selectAttribute() {
+        String jpql = "select p.description from Product p";
+        String jpqlClient = "select o.client from Order o";
 
+        TypedQuery<String> query = em.createQuery(jpql, String.class);
+        List<String> resultList = query.getResultList();
+
+        TypedQuery<Client> queryClient = em.createQuery(jpqlClient, Client.class);
+        List<Client> clientResultList = queryClient.getResultList();
+
+        Assert.assertTrue(Client.class.equals(clientResultList.get(0).getClass()));
+        Assert.assertTrue(String.class.equals(resultList.get(0).getClass()));
     }
 }
