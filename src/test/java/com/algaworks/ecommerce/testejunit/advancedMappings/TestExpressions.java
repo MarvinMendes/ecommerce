330898e328544fc1c2547c5c68cc3acb80bace66
Marvin Mendes <marvin.m.lopes@gmail.com>
2020-08-09 11:23:24 -0300

    Consulta com datas

---
+++ src/test/java/com/algaworks/ecommerce/testejunit/advancedMappings/TestExpressions.java
@@ -0,0 +1,15 @@
+package com.algaworks.ecommerce.testejunit.advancedMappings;
+
+import com.algaworks.ecommerce.EntityManagerTest;
+import org.junit.Test;
+
+public class TestExpressions extends EntityManagerTest {
+
+    @Test
+    public void testWithDate() {
+
+
+
+    }
+
+}
