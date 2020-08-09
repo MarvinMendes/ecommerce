61347bd38393cc1b0b966e9622c01e23d9a08fef
Marvin Mendes <marvin.m.lopes@gmail.com>
2020-08-09 15:26:39 -0300

    Consulta com o between

--- src/test/java/com/algaworks/ecommerce/testejunit/advancedMappings/TestExpressions.java
+++ src/test/java/com/algaworks/ecommerce/testejunit/advancedMappings/TestExpressions.java
@@ -1,10 +1,12 @@
 package com.algaworks.ecommerce.testejunit.advancedMappings;
 
 import com.algaworks.ecommerce.EntityManagerTest;
+import com.algaworks.ecommerce.model.product.Product;
 import org.junit.Assert;
 import org.junit.Test;
 
 import javax.persistence.TypedQuery;
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -22,5 +24,21 @@ public class TestExpressions extends EntityManagerTest {
         Assert.assertFalse(resultList.isEmpty());
 
     }
+
+    @Test
+    public void testBetween() {
+        String jpql = "select p from Product p where p.value between :min and :max";
+
+        TypedQuery<Product> query = em.createQuery(jpql, Product.class);
+        query.setParameter("min", new BigDecimal(499));
+        query.setParameter("max", new BigDecimal(1500));
+
+        List<Product> resultList = query.getResultList();
+
+        Assert.assertFalse(resultList.isEmpty());
+
+    }
+
+
 
 }
